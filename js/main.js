c058c52a93072d793bb38b9e121c9aaf6b852b32
LemmyOnukwugha <114013405+LemmyOnukwugha@users.noreply.github.com>
2023-05-11 19:21:17 -0400

    my dev skills lab

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$("#skills tbody").on("click", "button", function () {
+  $(this)
+    .closest("tr")
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+});
+
+$("#submit").on("click", function (evt) {
+  if ($('input[name="trait"]').val() === "") return;
+
+  let tempS = $('input[name="trait"]').val();
+
+  let newRow = `<tr>
+                    <td><button class="btn remove">X</button></td>
+                        <td>${tempS}</td>  
+                    </tr>`;
+
+  $("#skills tbody").append(newRow);
+});
